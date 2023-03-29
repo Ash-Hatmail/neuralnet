e4aff8dc5bef95383650bd060edeeef4e735cde8
V <aron.gjendem@gmail.com>
2023-03-29 10:03:29 +0200

    final??

--- src/matrix.rs
+++ src/matrix.rs
@@ -50,7 +50,6 @@ impl Matrix {
                 let mut sum = 0.0;
                 for k in 0..self.cols {
                     sum += self.data[i][k] * other.data[k][j];
-                
                 }
 
                 res.data[i][j] = sum;
@@ -67,13 +66,13 @@ impl Matrix {
 
         let mut res = Matrix::zeros(self.rows, self.cols);
 
-            for i in 0..self.rows {
-                for j in 0..other.cols {
-                    res.data[i][j] = self.data[i][j] + other.data[i][j];
+        for i in 0..self.rows {
+            for j in 0..other.cols {
+                res.data[i][j] = self.data[i][j] + other.data[i][j];
             }
         }
 
-    res
+        res
     }
 
     pub fn dot_multiply(&mut self, other: &Matrix) -> Matrix {
@@ -83,70 +82,68 @@ impl Matrix {
 
         let mut res = Matrix::zeros(self.rows, self.cols);
 
-            for i in 0..self.rows {
-                for j in 0..other.cols {
-                    res.data[i][j] = self.data[i][j] * other.data[i][j];
+        for i in 0..self.rows {
+            for j in 0..other.cols {
+                res.data[i][j] = self.data[i][j] * other.data[i][j];
             }
         }
 
         res
     }
 
-    pub fn subtract (&mut self, other: &Matrix) -> Matrix {
+    pub fn subtract(&mut self, other: &Matrix) -> Matrix {
         if self.rows != other.rows || self.cols != other.cols {
             panic!("Attempted to subtract matrix of incorrect dimensions")
         }
 
         let mut res = Matrix::zeros(self.rows, self.cols);
 
-            for i in 0..self.rows {
-                for j in 0..other.cols {
-                    res.data[i][j] = self.data[i][j] - other.data[i][j];
+        for i in 0..self.rows {
+            for j in 0..other.cols {
+                res.data[i][j] = self.data[i][j] - other.data[i][j];
             }
         }
 
         res
     }
     pub fn map(&mut self, function: &dyn Fn(f64) -> f64) -> Matrix {
-        
         Matrix::from(
-        (self.data)
-        .clone()
-        .into_iter()
-        .map(|row| row.into_iter().map(|value| function(value)).collect())
-        .collect(),
-    )
+            (self.data)
+                .clone()
+                .into_iter()
+                .map(|row| row.into_iter().map(|value| function(value)).collect())
+                .collect(),
+        )
     }
 
     pub fn transpose(&mut self) -> Matrix {
         let mut res = Matrix::zeros(self.cols, self.rows);
 
-        for i in 0..self.cols {
+        for i in 0..self.rows {
             for j in 0..self.cols {
-                res.data[i][j] = self.data[i][j]
-
+                res.data[j][i] = self.data[i][j]
             }
         }
-        
+
         res
     }
-}  
+}
 
 impl Debug for Matrix {
-	fn fmt(&self, f: &mut Formatter) -> Result {
-		write!(
-			f,
-			"Matrix {{\n{}\n}}",
-			(&self.data)
-				.into_iter()
-				.map(|row| "  ".to_string()
-					+ &row
-						.into_iter()
-						.map(|value| value.to_string())
-						.collect::<Vec<String>>()
-						.join(" "))
-				.collect::<Vec<String>>()
-				.join("\n")
-		)
-	}
+    fn fmt(&self, f: &mut Formatter) -> Result {
+        write!(
+            f,
+            "Matrix {{\n{}\n}}",
+            (&self.data)
+                .into_iter()
+                .map(|row| "  ".to_string()
+                    + &row
+                        .into_iter()
+                        .map(|value| value.to_string())
+                        .collect::<Vec<String>>()
+                        .join(" "))
+                .collect::<Vec<String>>()
+                .join("\n")
+        )
+    }
 }
